5cd46a7a0665188ec95d9ab1785bf48c0cff92ff
David Castro <davidernesto.castroanaya@gmail.com>
2023-11-04 12:21:16 -0600

    Initializing blog project

---
+++ 06_Blog/src/users/users.router.js
@@ -0,0 +1,25 @@
+const usersServices = require('./users.services');
+const router = require('express').Router();
+const passport = require('passport');
+const adminValidate = require('../middlewares/rol.middleware');
+
+require('../middlewares/auth.middleware')(passport)
+
+//? Main Route
+
+router.get('/',usersServices.getAllUsers);
+
+//? Protected routes for each user
+router.route('/me')
+    .get(passport.authenticate('jwt', {session:false}), usersServices.getMyUser)
+    .patch(passport.authenticate('jwt', {session: false}), usersServices.patchMyUser)
+    .delete(passport.authenticate('jwt', {session: false}), usersServices.deleteMyUser)
+
+//? Dynamic Routes by Id, restricted by middleware validation (adminValidate) if user is admin (passport.authenticate sets the req.user with the decrypted token info)
+router.route('/:id')
+    .get(usersServices.getUserById)
+    .patch(passport.authenticate('jwt', {session: false}),adminValidate,usersServices.patchUser)
+    .delete(passport.authenticate('jwt', {session: false}),adminValidate,usersServices.deleteUser)
+
+
+module.exports = router
