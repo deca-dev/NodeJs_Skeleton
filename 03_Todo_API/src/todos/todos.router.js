a123cd2d9909a22c49fc031baaef19f28cab6fba
David Castro <davidernesto.castroanaya@gmail.com>
2023-11-07 22:04:25 -0600

    Delete 03_Todo_API directory

--- 03_Todo_API/src/todos/todos.router.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const todoServices = require('./todos.services');
-
-router.get('/todos', todoServices.getTodos);
-router.post('/todos', todoServices.createNewTodo);
-router.get('/todos/:id', todoServices.getOneTodo);
-
-module.exports = router;
